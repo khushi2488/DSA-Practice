6638cdcb9d69d0de5538b93cb29956d5bc3206c8
Khushi Patel <khushiiip2004@gmail.com>
2026-01-18 11:37:35 +0530

    kth missing number in log(n)

--- Binary Search On Answer/kthmissingnum.cpp
+++ Binary Search On Answer/kthmissingnum.cpp
@@ -16,6 +16,50 @@ class Solution{
   }
   return curr-1;
   }
-};
 // tc : O(n+k)
 //sc : O(1)
+
+int findmissing2(vector<int>& arr, int k){
+int num=1;
+int n=arr.size();
+int i=0;
+while(i<n && k>0){
+    if(arr[i]==num){
+        i++;
+    }
+    else {
+        k--;
+    }
+    num++;
+}
+while(k--){
+    num++;
+}
+return num-1;
+  }
+
+
+
+  int findmissing3(vector<int>& nums, int k ){
+    int n=nums.size();
+    int l=0;
+    int r=n-1;
+    while(l<=r){
+        int mid=l+(r-l)/2;
+        int kthmissing=nums[mid]-(mid+1);
+        if(kthmissing<k){
+            l=mid+1;
+        }
+        else{
+            r=mid-1;
+        }
+
+    }
+    return l+k;
+  }
+//O(logn)
+
+
+};
+
+
