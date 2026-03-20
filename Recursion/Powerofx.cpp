ef0699637e52aab73a89581113707d583ba7e1fb
Khushi Patel <khushiiip2004@gmail.com>
2026-03-20 22:15:55 +0530

    power of x , power of number is it's reverse num

--- Recursion/Powerofx.cpp
+++ Recursion/Powerofx.cpp
@@ -1,5 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-int reversenumber(int n, int rev=0){
-    
+//power of x
+int power(int x, int n){
+    if(n==0){
+        return 1;
+    }
+    return x*power(x,n-1);
 }
+int reversenumber(int n, int rev=0){
+  if(n==0){
+    return rev;
+  }   
+  return reversenumber(n/10,rev*10+(n%10));
+}
+long long power2(int x, int n){
+    if(n==0){
+        return 1;
+    }
+    long long half=power(x,n/2);
+    if(n%2==0){
+        return half*half;
+    }
+    else{
+        return x*half*half;
+    }
+}
+//reverse : O(n)
+// Power: O(logn)
