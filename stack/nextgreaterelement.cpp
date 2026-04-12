28cee083252929f31b641001c3c78b45533621d8
Khushi Patel <khushiiip2004@gmail.com>
2026-04-12 21:58:34 +0530

    next greater ele using stack

--- stack/nextgreaterelement.cpp
+++ stack/nextgreaterelement.cpp
@@ -13,3 +13,26 @@ vector<int> nextgreaterele(vector<int>& arr){
         return res;
     }
 }
+// TC : O(n^2)
+//sc : O(n)
+
+vector<int> nextgreaterelement2(vector<int>& arr){
+    int n=arr.size();
+    vector<int> res(n);
+    stack<int> st;
+    for(int i=n-1;i>=0;i--){
+        while(!st.empty() && st.top()<=arr[i]){
+            st.pop();
+        }
+        if(st.empty()){
+            res[i]=-1;
+        }
+        else{
+            res[i]=st.top();
+        }
+        st.push(arr[i]);
+    }
+    return res;
+}
+//tc : O(n)
+//Sc : O(n)
