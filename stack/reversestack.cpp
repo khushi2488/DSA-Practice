bc77094bee2f3b52bd1b6d3a69c9c56b29ad9075
Khushi Patel <khushiiip2004@gmail.com>
2026-04-14 00:04:48 +0530

    reversestack

---
+++ stack/reversestack.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+  public:
+    void reverseStack(stack<int> &st) {
+        // code here
+        int n=st.size();
+        vector<int>res;
+        while(!st.empty()){
+            res.push_back(st.top());
+            st.pop();
+        }
+        for(int i=0;i<res.size();i++){
+            st.push(res[i]);
+        }
+        
+        
+        
+        
+    }
+
+void reversestack(stack<int>& s){
+    stack<int> temp;
+    while(!s.empty()){
+    temp.push(s.top());
+    s.pop();
+    }
+s=temp;
+}
+
+
+};
