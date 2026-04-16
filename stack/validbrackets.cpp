1f2235a507a3df9a4728f4982262cfc8e1bbd227
Khushi Patel <khushiiip2004@gmail.com>
2026-04-16 22:46:35 +0530

    valid brackets

---
+++ stack/validbrackets.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isvalidbrackets(string expression)
+{
+    stack<char> s;
+    for (int i = 0; i < expression.length(); i++)
+    {
+        char ch = expression[i];
+        if (ch == '(' || ch == '{' || ch == '[')
+        {
+            s.push(ch);
+        }
+        else
+        {
+            if (!s.empty())
+            {
+                char top = s.top();
+                if ((ch == ')' && top == '(') ||
+                    (ch == '}' && top == '{') ||
+                    (ch == ']' && top == '['))
+                {
+                    s.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+    }
+
+        if(s.empty()){
+            return true;
+        }
+        else{
+            return false;
+        }
+    
+}
+
+//tc : O(n)   Sc : O(n)
